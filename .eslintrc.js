c406e0f6f01fc2487ba1068bcbdc3a5b701b5e6b
Guany <1084072690@qq.com>
2022-11-23 16:58:46 +0800

    chore: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,14 @@
 module.exports = {
-  extends: ['vuepress'],
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
   rules: {
     'prettier/prettier': 'warn',
   },
